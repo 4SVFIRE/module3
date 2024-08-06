08b003fa7d1493c76376fb8be4a74cf8579d20ca
FIRE <133657078+4SVFIRE@users.noreply.github.com>
2024-08-06 22:53:32 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -1,18 +1,17 @@
 def count_calls():
     global calls
+    calls += 1
     return calls
 
 
 def string_info(string):
-    global calls
-    calls += 1
+    count_calls()
     string = (len(string), string.upper(), string.lower())
     return string
 
 
 def is_contains(string, list_to_search):
-    global calls
-    calls += 1
+    count_calls()
     string = string.lower()
     list_to_search = [i.lower() for i in list_to_search]
     return string in list_to_search
